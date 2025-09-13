45f5b813f7d549d5228c8df20023191b683e0246
Meghali54 <meghalidutta78@gmail.com>
2025-09-13 11:45:27 +0530

    Added new streamlit app and requirements.txt file

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import streamlit as st
 import requests
-from vaderSentiment import SentimentIntensityAnalyzer  # FIXED IMPORT
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
 import nltk
 
 # Download VADER lexicon only if not present
